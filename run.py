7ec59b3fa3ae818dd2cac04d3deab4eb4dacadf1
Barry Fitzachary <Barryfitzachary@gmail.com>
2023-08-06 22:27:24 +0000

    Add validation to check for exactly 6 values in provided data form the user. show error when anything but 6 numbers entered.

--- run.py
+++ run.py
@@ -12,15 +12,35 @@ SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("PLease enter sales data from the last market.")
+    print("Please enter sales data from the last market.")
     print("Data should be six numbers, seperated by commas.")
     print("Example: 10,20,30,40,50,60")
 
     data_str = input("Enter your data here: ")
-    print(f"The Data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exacty 6 values.
+    """
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f"Exactly 6 Values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid Data: {e}, Please try again. \n")
+
+
 
 get_sales_data()
